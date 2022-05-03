750fb0a58adadcc478d43d73bddcc6fc6df5b20e
vozle <vozle2@gmail.com>
2022-05-03 22:58:30 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/JsonSchemaTest.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class JsonSchemaTest {
+    @Test
+    void shouldSendSomeData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
