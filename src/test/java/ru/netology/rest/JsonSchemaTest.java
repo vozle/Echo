55bda4ddb24083a482936b04d55ed9f304139843
vozle <vozle2@gmail.com>
2022-05-03 23:04:22 +0300

    Fix test for fail

--- src/test/java/ru/netology/rest/JsonSchemaTest.java
+++ src/test/java/ru/netology/rest/JsonSchemaTest.java
@@ -19,7 +19,7 @@ public class JsonSchemaTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("sum data"))
         ;
     }
 }
