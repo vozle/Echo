dbfec52b340ae8677c1c71176a7273848cfeee31
vozle <vozle2@gmail.com>
2022-05-03 23:06:40 +0300

    Fix test for pass

--- src/test/java/ru/netology/rest/JsonSchemaTest.java
+++ src/test/java/ru/netology/rest/JsonSchemaTest.java
@@ -19,7 +19,7 @@ public class JsonSchemaTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("sum data"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
